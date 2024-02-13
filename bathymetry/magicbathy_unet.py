c7cf16be546c5d1257aa4d74ba8ca80a8bc778cf
Panagiotis Agrafiotis <agrafiotis.panagiotis@gmail.com>
2024-02-13 15:52:49 +0100

    Update and rename MagicBathy-U-Net.py to magicbathy_unet.py

--- bathymetry/magicbathy_unet.py
+++ bathymetry/magicbathy_unet.py
@@ -9,7 +9,7 @@ even with limited annotated data.
 
 If you use this code please cite: ...
 
-MagicBathy-U-Net.py © 2024 by Panagiotis Agrafiotis is licensed under Attribution-NonCommercial-ShareAlike 4.0 International 
+magicbathy_unet.py © 2024 by Panagiotis Agrafiotis is licensed under Attribution-NonCommercial-ShareAlike 4.0 International 
 
 This license requires that reusers give credit to the creator. It allows reusers 
 to distribute, remix, adapt, and build upon the material in any medium or format,
